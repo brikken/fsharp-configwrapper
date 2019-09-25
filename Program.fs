d23691a35f0c741bb4551dbfc88123fee02e8bf0
Mads Nielsen <post@madsnielsen.dk>
2019-09-25 21:48:16 +0200

    upgrade DTO version

--- Program.fs
+++ Program.fs
@@ -6,22 +6,46 @@ open SimpleSerialization
 open Microsoft.FSharpLu.Json
 open VersionedDTOs
 
+let serialize serializer path contents name =
+    try    
+        contents |> serializer |> fun c -> System.IO.File.WriteAllText(path, c)
+        printfn "Serialized %s" name
+    with
+        ex -> printfn "%s serialization failed: %s" name ex.Message    
+
 [<EntryPoint>]
 let main argv =
-    // [ ("test.txt", "Hej med dig"); ("?\\", "This won't save!") ]
-    //     |> List.iter (fun (path, contents) ->
-    //         match (writeToFile path contents) with
-    //             | Ok msg -> sprintf "OK: %s" msg
-    //             | Error msg -> sprintf "Error: %s" msg
-    //         |> printfn "%s")
+    [ ("test.txt", "Hej med dig"); ("?\\", "This won't save!") ]
+        |> List.iter (fun (path, contents) ->
+            match (writeToFile path contents) with
+                | Ok msg -> sprintf "OK: %s" msg
+                | Error msg -> sprintf "Error: %s" msg
+            |> printfn "%s")
 
-    // List.iter (fun f ->
-    //     f { text = "Noget skør dansk LÅLÅ tekst! <bløb>"; value = 12; items = [ OptionOne; OptionTwo ("test", 14); OptionThree (12.34) ] }
-    //     |> printfn "%s") [ getXml; Default.serialize; Compact.serialize ]
-    
-    try    
-        { name = "Test Testersen"; age = 34; boss = Some "Bossy Bosson" } |> Compact.serialize |> fun c -> System.IO.File.WriteAllText("DTO1.json", c)
-        printfn "Serialized DTO1"
+    List.iter (fun f ->
+        f { text = "Noget skør dansk LÅLÅ tekst! <bløb>"; value = 12; items = [ OptionOne; OptionTwo ("test", 14); OptionThree (12.34) ] }
+        |> printfn "%s") [ getXml; Default.serialize; Compact.serialize ]
+
+    let compactSerializer = serialize Compact.serialize
+
+    let dto1File = "DTO1.json"
+    serialize Compact.serialize dto1File { DTO1.name = "Test Testersen"; age = 34; boss = Some "Bossy Bosson" } "DTO1"
+
+    let (dto1: DTO1) = System.IO.File.ReadAllText(dto1File) |> Compact.deserialize
+    match dto1.boss with
+        | None -> printfn "No boss"
+        | Some boss -> printfn "The boss is %s" boss
+
+    let dto2 = getDTO2 dto1
+    let dto2New = { dto2 with boss = "Mr. Leader" :: dto2.boss }
+    dto2New |> Compact.serialize |> printfn "%s"
+
+    let dto2File = "DTO2.json"
+    serialize Compact.serialize dto2File dto2New "DTO2"
+
+    try
+        System.IO.File.ReadAllText(dto1File) |> Compact.deserialize<DTO2> |> ignore
     with
-        ex -> printfn "DTO1 serialization failed: %s" ex.Message    
+        ex -> printfn "%s is not of type DTO2: %s" dto1File ex.Message
+
     0 // return an integer exit code
--- VersionedDTOs.fs
+++ VersionedDTOs.fs
@@ -5,3 +5,12 @@ type DTO1 = {
     age: int
     boss: string option
 }
+
+type DTO2 = {
+    name: string
+    age: int
+    boss: string list
+}
+
+let getDTO2 (dto1: DTO1) =
+    { DTO2.name = dto1.name; age = dto1.age; boss = match dto1.boss with None -> [] | Some boss -> [boss] }
