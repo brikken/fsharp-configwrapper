c12709b8b7a1a4a51031339b2fb8dcc07e25e625
Mads Nielsen <post@madsnielsen.dk>
2019-10-15 17:42:15 +0200

    large binary serialization

--- BinarySerialization.fs
+++ BinarySerialization.fs
@@ -21,3 +21,19 @@ let serialize x =
     let stream = new IO.MemoryStream()
     formatter.Serialize(stream, x)
     stream.ToArray() |> System.Text.Encoding.UTF8.GetString
+
+let getSomeType n =
+    match (n % 4) with
+    | 0 -> CaseA
+    | 1 -> CaseB "This is a case B"
+    | 2 -> CaseC (DateTime.Now, 123.45m)
+    | 3 -> CaseD { value = 12; name = "This is a name"; }
+    | _ -> failwith "not possible"
+
+let getSomeTypes n =
+    Seq.map getSomeType [0..n]
+
+let serializeToFile path n =
+    let stream = IO.File.OpenWrite(path)
+    formatter.Serialize(stream, getSomeTypes n)
+    stream.Close()
--- Program.fs
+++ Program.fs
@@ -1,23 +1,9 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-open SimpleIO
-open SimpleSerialization
-open Microsoft.FSharpLu.Json
-open VersionedDTOs
 open BinarySerialization
-
-let serialize serializer path contents name =
-    try    
-        contents |> serializer |> fun c -> System.IO.File.WriteAllText(path, c)
-        printfn "Serialized %s" name
-    with
-        ex -> printfn "%s serialization failed: %s" name ex.Message    
 
 [<EntryPoint>]
 let main argv =
-    printfn "%s" (BinarySerialization.serialize CaseA)
-    printfn "%s" (BinarySerialization.serialize (CaseB "test"))
-    printfn "%s" (BinarySerialization.serialize (CaseC (DateTime(2000,1,1), 10.2M)))
-    printfn "%s" (BinarySerialization.serialize (CaseD { value = 10; name = "something"; }))
+    serializeToFile "test.bin" 10000000
     0 // return an integer exit code
